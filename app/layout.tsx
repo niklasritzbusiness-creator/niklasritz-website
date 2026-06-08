c77b47eabf352d5e110e8a7de65235c5f6f0614b
niklasritzjp-sys <niklasritzjp@gmail.com>
2026-06-08 15:46:17 +0200

    Speedinsight

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">
+        <SpeedInsights />
+        {children}
+      </body>
     </html>
   );
 }

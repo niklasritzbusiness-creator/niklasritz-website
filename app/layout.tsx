53629c68ab52b61a9c2617e70d0421b00ceeec7f
niklasritzjp-sys <niklasritzjp@gmail.com>
2026-06-08 16:02:18 +0200

    Deleted Speedinsight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
-        <SpeedInsights />
         {children}
       </body>
     </html>
